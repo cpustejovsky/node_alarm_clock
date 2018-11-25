11378f7c377250d1109c42cc2740d9285a1cd4e6
cpustejovsky <cpustejovsky@bitpay.com>
2018-11-25 15:56:32 -0500

    created random array generator in alarm.js; scrapped mp3Player.js and its dependencies and starting from scratch

--- alarm.js
+++ alarm.js
@@ -1,3 +1,25 @@
 "use strict"
+/*  STRATEGY
+STEP ONE
+    Based on the list of mp3s within the mp3 folder, the script will play them in order.
 
-//ask for user input
+/*
+Generates random list between 1 and the length of the provided audio list.
+The random list will create the order in which the mp3 files will be played.
+*/
+
+let myaudiolist = [1,2,3,4]; //sample data
+
+function genereteRandomList (audiolist){
+    let orderlist = [];
+    let maxlength = audiolist.length + 1;
+    while (orderlist.length < audiolist.length) {
+        let randomNumber = Math.floor(Math.random() * (+maxlength - 1)) + 1;
+        if (randomNumber !== 0 && randomNumber !== orderlist[3] && randomNumber !== orderlist[2] && randomNumber !== orderlist[1] && randomNumber !== orderlist[0]) {
+            orderlist.push(randomNumber);
+        }
+    }
+    return orderlist
+}
+
+console.log(genereteRandomList(myaudiolist));
