0610a300fe3f99db385bd27a84c2697a02551b78
cpustejovsky <charles.pustejovsky@gmail.com>
2020-01-12 14:48:21 -0500

    implemented prompts to provide a better UI for the alarm; also can be set for AM and PM and uses 12 hr instead of 24 hr clock.

--- alarm.js
+++ alarm.js
@@ -1,46 +1,91 @@
-const spawn = require('child_process').spawn;
-const moment = require('moment');
-// const prompts = require('prompts');
+const spawn = require("child_process").spawn;
+const moment = require("moment");
+const prompts = require("prompts");
 //TODO: Make use of prompts
 //TODO: Test with lofi hiphop and something else at the office and kill two birds with one stone
-const brownNoise = 'mp3/brown-noise.mp3';
-const alarm = 'mp3/yes-roundabout.mp3';
+const brownNoise = "mp3/brown-noise.mp3";
+const alarm = "mp3/yes-roundabout.mp3";
 
-fuzzyNoise = spawn('mplayer', ['-slave', brownNoise]);
-fuzzyNoise.stdout.on('data', function (data) { console.log('fuzzyNoise stdout: ' + data); });
-fuzzyNoise.stderr.on('data', function (data) { console.log('fuzzyNoise stderr: ' + data); });
-fuzzyNoise.on('exit', function () {
-    console.log('fuzzy noise exited.');
-});
+const playFuzzyNoise = () => {
+  fuzzyNoise = spawn("mplayer", ["-slave", brownNoise]);
+  fuzzyNoise.stdout.on("data", function(data) {
+    console.log("fuzzyNoise stdout: " + data);
+  });
+  fuzzyNoise.stderr.on("data", function(data) {
+    console.log("fuzzyNoise stderr: " + data);
+  });
+  fuzzyNoise.on("exit", function() {
+    console.log("fuzzy noise exited.");
+  });
+};
 
-const time = process.argv[2].split(':');
+const setWakeUpTime = async function() {
+  const questions = [
+    {
+      type: "number",
+      name: "hour",
+      message: "Set Hour",
+      validate: hour =>
+        13 > hour > 0 ? true : "Please enter a correct hour (1-12)"
+    },
+    {
+      type: "number",
+      name: "minute",
+      message: "Set Minute",
+      validate: minute =>
+        61 > minute > -1 ? true : "Please enter a correct minute (0-60)"
+    },
+    {
+      type: "select",
+      name: "meridiem",
+      message: "AM or PM?",
+      choices: [{ value: "AM" }, { value: "PM" }]
+    }
+  ];
+
+  const response = await prompts(questions);
+  if (response) {
+    playFuzzyNoise();
+    console.log(`Setting alarm for ${response.hour}:${response.minute} ${response.meridiem}`);
+    checkTime(response);
+  }
+};
+
 let sleep = true;
 
-function checkTime() {
-    if (sleep) {
-        var now = moment();
-        if (now.hours() >= time[0] && now.minutes() >= time[1] && now.format("A") === "AM") {
-            wakeUp()
-        } else {
-            console.log(now.format("LTS"));
-            setTimeout(checkTime, 1000 * 10);
-        }
+function checkTime(time) {
+  if (sleep) {
+    var now = moment();
+    if (
+      Number(now.format("hh")) >= time.hour &&
+      Number(now.format("mm")) >= time.minute &&
+      now.format("A") === time.meridiem
+    ) {
+      wakeUp();
+    } else {
+      console.log(now.format("LTS"));
+      setTimeout(function() {
+        checkTime(time);
+      }, 1000);
     }
+  }
 }
 
 function wakeUp() {
-    console.log("fuzzy noise should exit");
-    fuzzyNoise.kill();
-    sleep = false;
-    console.log("wake up!")
-    alarmClock = spawn('mplayer', ['-slave', alarm]);
-    alarmClock.stdout.on('data', function (data) { console.log('alarmClock stdout: ' + data); });
-    alarmClock.stderr.on('data', function (data) { console.log('alarmClock stderr: ' + data); });
-    alarmClock.on('exit', function () {
-        console.log('EXIT.');
-    });
+  console.log("fuzzy noise should exit");
+  fuzzyNoise.kill();
+  sleep = false;
+  console.log("wake up!");
+  alarmClock = spawn("mplayer", ["-slave", alarm]);
+  alarmClock.stdout.on("data", function(data) {
+    console.log("alarmClock stdout: " + data);
+  });
+  alarmClock.stderr.on("data", function(data) {
+    console.log("alarmClock stderr: " + data);
+  });
+  alarmClock.on("exit", function() {
+    console.log("EXIT.");
+  });
 }
 
-console.log("Setting alarm for: " + time.join(":"));
-
-checkTime();
+setWakeUpTime();
