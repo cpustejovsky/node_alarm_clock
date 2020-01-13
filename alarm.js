9801b067ac49fbf3c62a6592ff0025a1507c0e97
cpustejovsky <charles.pustejovsky@gmail.com>
2020-01-13 07:52:03 -0500

    no longer console logging every stdout and stderr; this was at my wife's request since she thought the terminal could look nicer when the alarm script was running

--- alarm.js
+++ alarm.js
@@ -4,14 +4,18 @@ const prompts = require("prompts");
 //TODO: make use of https://github.com/noodny/mplayer
 const brownNoise = "mp3/brown-noise.mp3";
 const alarm = "mp3/yes-roundabout.mp3";
-
+let sleep = true;
 const playFuzzyNoise = () => {
   fuzzyNoise = spawn("mplayer", ["-slave", brownNoise]);
   fuzzyNoise.stdout.on("data", function(data) {
-    console.log("fuzzyNoise stdout: " + data);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write("fuzzyNoise stdout: " + data);
   });
   fuzzyNoise.stderr.on("data", function(data) {
-    console.log("fuzzyNoise stderr: " + data);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write("fuzzyNoise stderr: " + data);
   });
   fuzzyNoise.on("exit", function() {
     console.log("fuzzy noise exited.");
@@ -52,17 +56,17 @@ const setWakeUpTime = async function() {
   }
 };
 
-let sleep = true;
-
 function checkTime(time) {
   if (sleep) {
     let now = moment();
     if (
-      Number(now.format("hh")) === time.hour &&
-      Number(now.format("mm")) === time.minute &&
+      Number(now.format("hh")) >= time.hour &&
+      Number(now.format("mm")) >= time.minute &&
       now.format("A") === time.meridiem
     ) {
-      wakeUp();
+      if (Number(now.format("hh")) !== 12 && now.format("A") === "AM") {
+        wakeUp();
+      }
     } else {
       setTimeout(function() {
         checkTime(time);
@@ -78,10 +82,14 @@ function wakeUp() {
   console.log("wake up!");
   alarmClock = spawn("mplayer", ["-slave", alarm]);
   alarmClock.stdout.on("data", function(data) {
-    console.log("alarmClock stdout: " + data);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write("Alarm clock stdout: " + data);
   });
   alarmClock.stderr.on("data", function(data) {
-    console.log("alarmClock stderr: " + data);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write("Alarm clock stderr: " + data);
   });
   alarmClock.on("exit", function() {
     console.log("EXIT.");
